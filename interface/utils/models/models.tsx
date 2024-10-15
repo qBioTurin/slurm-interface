0ba417c5d8bb8d999348c018083268a6c5f8177f
Elisa Li Perottino <elisa.perottino@edu.unito.it>
2024-10-15 12:00:22 +0200

    feat(jobs): update data model

--- interface/utils/models/models.tsx
+++ interface/utils/models/models.tsx
@@ -19,30 +19,28 @@ export interface Node {
   reason: string;
 }
 
+export type JobState = keyof typeof JobStateDescriptions;
+
+export const JobStateDescriptions = {
+  PD: 'Pending: The job is waiting in a queue for allocation of resources',
+  R: 'Running: The job currently is allocated to a node and is running',
+  CG: 'Completing: The job is finishing but some processes are still active',
+  CD: 'Completed: The job has completed successfully',
+  F: 'Failed: The job failed with a non-zero exit value',
+  TO: 'Terminated: Job terminated by Slurm after reaching its runtime limit',
+  S: 'Suspended: A running job has been stopped with its resources released to other jobs',
+  ST: 'Stopped: A running job has been stopped with its resources retained',
+} as const;
 
 export interface Job {
-  JobID: number;
-  JobName: string;
-  User: string;
-  Partition: string;
-  State:
-  | 'COMPLETED'
-  | 'COMPLETING'
-  | 'FAILED'
-  | 'PENDING'
-  | 'PREEMPTED'
-  | 'RUNNING'
-  | 'SUSPENDED'
-  | 'STOPPED'
-  TimeSubmit: string;
-  TimeStart?: string;
-  Nodes: number;
-  CPUs: number;
-  Memory: string;
-  TimeLimit: string;
-  TimeUsed?: string;
-  TimeLeft?: string;
-  Priority: number;
+  jobId: number;
+  partition: string;
+  name: string;
+  user: string;
+  state: JobState;
+  time: string;
+  nodesCount: number;
+  nodeList: Node[];
 }
 
 export interface Reservation {
