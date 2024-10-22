ac1be54cfa093d9f123b1d125bb85807aac462d3
Elisa Li Perottino <elisa.perottino@edu.unito.it>
2024-10-22 23:26:14 +0200

    feat(schemas): add zod schema for job submissions

---
+++ interface/src/app/schemas/job_submission_schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const EnvironmentSchema = z.object({
+  PATH: z.string().optional(),
+});
+
+export const JobSubmissionSchema = z.object({
+    tasks: z.number().min(1, 'Number of tasks is required'),
+    name: z.string().min(1, 'Job name is required'),
+    nodes: z.number().min(1, 'Number of nodes is required'),
+    current_working_directory: z.string(),
+    environment: EnvironmentSchema,
+    script: z.string().min(1, 'Command line is required'),
+    // description: z.string().optional(),
+    // partition: z.string().optional(),
+    // specify_nodes: z.string().optional(),
+    // immediate: z.boolean().optional(),
+    // tmp_disk_space: z.number().optional(),
+    // min_memory_per_cpu: z.number().optional(),
+    // cpus_per_task: z.number().optional(),
+    // tasks_per_node: z.number().optional(),
+  });
