49a5bc0b5fa03fbda668ca4502f7a80176b48be1
Elisa Li Perottino <elisa.perottino@edu.unito.it>
2024-10-23 14:36:20 +0200

    feat(reservations): add react-hook-form and zod validation to submission form

--- interface/src/app/components/reservations/new/NewReservationForm.tsx
+++ interface/src/app/components/reservations/new/NewReservationForm.tsx
@@ -3,55 +3,57 @@ import { useState } from 'react';
 import { mockNodes } from '../../../../../utils/models/mock';
 import { DateInput, TimeInput } from '@mantine/dates';
 import { Stepper, Button, Group, TextInput, TagsInput, NumberInput, MultiSelect, Code, rem } from '@mantine/core';
-import { useForm } from '@mantine/form';
 import { IconCalendar, Icon } from '@tabler/icons-react';
 import dayjs from 'dayjs';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {ReservationSubmissionSchema} from '@/schemas/reservation_submission_schema';
 
-
-
+type ReservationSubmissionSchema = z.infer<typeof ReservationSubmissionSchema>;
 
 export default function NewReservationForm() {
     const [active, setActive] = useState(0);
-    const [additionalFields, setAdditionalFields] = useState<{ id: number; field1: string; field2: string }[]>([]);
+    // const [additionalFields, setAdditionalFields] = useState<{ id: number; field1: string; field2: string }[]>([]);
 
-
-    const form = useForm({
-        mode: 'uncontrolled',
-        initialValues: {
+    const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm<ReservationSubmissionSchema>({
+        resolver: zodResolver(ReservationSubmissionSchema),
+        defaultValues: {
             name: '',
             StartTime: new Date(),
             EndTime: new Date(),
-            Users: new Array<string>(),
+            Users: [],
             NodeCnt: 0,
-            Nodes: new Array<string>(),
+            Nodes: [],
             PartitionName: '',
-        },
-
-        validate: (values) => {
-            return {};
-        },
-
-
+        }
     });
 
-    const addFields = () => {
-        setAdditionalFields((prev) => [
-            ...prev,
-            { id: prev.length, field1: '', field2: '' },
-        ]);
+    const validateTimes = (startTime: Date, endTime: Date) => {
+        const errors: Record<string, string> = {};
+
+        if (endTime < startTime) {
+            errors.EndTime = "End time must be greater than or equal to Start time.";
+        } else if (dayjs(endTime).isSame(startTime, 'day') && endTime <= startTime) {
+            errors.EndTime = "If the dates are the same, end time must be greater than start time.";
+        }
+
+        return errors;
     };
 
-    const handleFieldChange = (id: number, field: 'field1' | 'field2', value: string) => {
-        setAdditionalFields((prev) =>
-            prev.map((item) =>
-                item.id === id ? { ...item, [field]: value } : item
-            )
-        );
+    const onSubmit: SubmitHandler<ReservationSubmissionSchema> = (data) => {
+        const timeErrors = validateTimes(data.StartTime, data.EndTime);
+
+        if (Object.keys(timeErrors).length > 0) {
+            return;
+        }
+        console.log("Submitted Data:", data);
     };
+
 
     const nextStep = () =>
         setActive((current) => {
-            if (form.validate().hasErrors) {
+            if (Object.keys(errors).length > 0) {
                 return current;
             }
             return current < 2 ? current + 1 : current;
@@ -68,103 +70,95 @@ export default function NewReservationForm() {
                 >
 
                     <TextInput
-                        {...form.getInputProps('name')}
+                        {...register('name')}
                         label="Reservation Name"
                         placeholder="Name"
                         mt="md"
+                        error={errors.name?.message}
                     />
 
                     <DateInput
-                        {...form.getInputProps('StartTime')}
                         label="Start Time"
                         valueFormat="DD/MM/YYYY HH:mm:ss"
                         placeholder="Start Time"
-                        mt="md"
+                        {...register('StartTime')}
+                        error={errors.StartTime?.message}
                         withAsterisk
-                        dateParser={(s) =>
-                            dayjs(s, "DD/MM/YYYY HH:mm:ss").toDate().getTime()
-                                ? dayjs(s, "DD/MM/YYYY HH:mm:ss").toDate()
-                                : new Date(s)
-                        }
-                    />
-                    <DateInput
-                        {...form.getInputProps('EndTime')}
-                        valueFormat="DD/MM/YYYY HH:mm:ss"
-                        label="End Time"
-                        placeholder="End Time"
-                        dateParser={(s) =>
-                            dayjs(s, "DD/MM/YYYY HH:mm:ss",).toDate().getTime()
-                                ? dayjs(s, "DD/MM/YYYY HH:mm:ss").toDate()
-                                : new Date(s)
-                        }
-                        mt="md"
+                        onChange={(date) => {
+                            if (date) {
+                                setValue('StartTime', date);
+                            }
+                        }}
+                        dateParser={(s) => dayjs(s, "DD/MM/YYYY HH:mm:ss").toDate()}
                     />
 
+                    <DateInput
+                        label="End Time"
+                        valueFormat="DD/MM/YYYY HH:mm:ss"
+                        placeholder="End Time"
+                        {...register('EndTime')}
+                        error={errors.EndTime?.message}
+                        withAsterisk
+                        onChange={(date) => {
+                            if (date) {
+                                setValue('EndTime', date);
+                            }
+                        }}
+                        dateParser={(s) => dayjs(s, "DD/MM/YYYY HH:mm:ss").toDate()}
+                        mt="md"
+                    />
 
                     <TagsInput
-                        {...form.getInputProps('Users')}
+                        value={watch('Users') || []}
+                        onChange={(value) => setValue('Users', value as [string, ...string[]])}
                         label="Users"
                         mt="md"
                         clearable
+                        error={errors.Users?.message}
                     />
 
-
                     <MultiSelect
-                        {...form.getInputProps('Nodes')}
                         data={mockNodes.map((node) => node.nodeName)}
                         label="Nodes"
                         placeholder="Select nodes"
                         mt="md"
                         searchable
+                        value={watch('Nodes')}
+                        onChange={(value) => setValue('Nodes', value)}
+                        error={errors.Nodes?.message}
                     />
 
                     <NumberInput
-                        {...form.getInputProps('NodeCnt')}
                         label="Node Count"
+                        {...register('NodeCnt', { valueAsNumber: true })}
                         min={0}
+                        max={200} // arbitrary value
                         mt="md"
+                        error={errors.NodeCnt?.message}
+                        value={watch('NodeCnt') ?? 0}
+                        onChange={(value) => {
+                            if (typeof value === 'number') {
+                                setValue('NodeCnt', value);
+                            } else {
+                                setValue('NodeCnt', 0);
+                            }
+                        }}
                     />
 
                     <TextInput
-                        {...form.getInputProps('PartitionName')}
+                        {...register('PartitionName')}
                         label="Partition Name"
                         placeholder="Partition Name"
-                        mt="md" />
-
-
-                    {additionalFields.map((field) => (
-
-                        <Group key={field.id} mt="md" grow>
-                            <TextInput
-                                {...form.getInputProps(field.field1)}
-                                key={form.key('field1')}
-                                label="Option"
-                                value={field.field1}
-                                onChange={(event) => handleFieldChange(field.id, 'field1', event.currentTarget.value)}
-                                placeholder="Option"
-                            />
-                            <TextInput
-                                {...form.getInputProps(field.field2)}
-                                key={form.key('field2')}
-                                label="Value"
-                                value={field.field2}
-                                onChange={(event) => handleFieldChange(field.id, 'field2', event.currentTarget.value)}
-                                placeholder="Value"
-                            />
-                        </Group>
-                    ))}
-
-                    <Button onClick={addFields} mt="md">
-                        Add Fields
-                    </Button>
-
+                        mt="md" 
+                        error={errors.PartitionName?.message}    
+                    />
 
                 </Stepper.Step>
 
                 <Stepper.Completed>
                     Completed! Form values:
                     <Code block mt="xl">
-                        {JSON.stringify(form.getValues(), null, 2)}
+                        {JSON.stringify(watch(), null, 2)}
                     </Code>
                 </Stepper.Completed>
             </Stepper >
@@ -175,7 +169,12 @@ export default function NewReservationForm() {
                         Back
                     </Button>
                 )}
-                {active !== 3 && <Button onClick={nextStep}>Done</Button>}
+                
+                {active !== 3 && (
+                    <Button onClick={handleSubmit(onSubmit)}>
+                        Done
+                    </Button>
+                )}
             </Group>
         </>
     );
