d39ebdf9fe5ac7a10ad8fc73920098fab1caec8d
Este Hoyos <estehoyos@gmail.com>
2024-11-27 21:44:08 +0100

    style: sign in card

--- interface/src/components/login/LoginForm.tsx
+++ interface/src/components/login/LoginForm.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { Card, Button, TextInput, Group, Text, rem, Title, Center } from '@mantine/core';
+import { Card, Button, TextInput, Group, Text, Stack, Title, Center } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconAt } from '@tabler/icons-react';
 import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import logo from "@/assets/logo_hpc4ai.png"
 
 export default function LoginForm() {
     const router = useRouter();
@@ -22,36 +24,38 @@ export default function LoginForm() {
 
     return (
         <>
-            <Card shadow="sm" radius="md" withBorder padding='xl'>
-                <Center>
-                    <Title order={1} mb='md'>Sign in with your account</Title>
-                </Center>
-                <TextInput
-                    mt="md"
-                    placeholder="Email"
-                    width='300'
-                    label='Email'
-                    key={form.key('email')}
-                    error={form.errors.email}
-                    styles={{ input: { borderColor: form.errors.email ? 'red' : undefined } }}
-                    {...form.getInputProps('email')}
+            <Card shadow="xl" radius="md" withBorder padding='xl' >
+                <Stack justify='center' align='center' m='xl' >
+                    <Image src={logo} alt="HPC4AI" height={70} />
+                    <Group><Title fw='900' size='40' mt='md' mb='md'>Sign</Title><Title fw='900' size='40' c='#A41517'>in</Title> </Group>
+                    <Text c='dimmed' mb='1' lineClamp={2} h='21' >Enter your email and password</Text>
+                    <Text c='dimmed' mb='md' p='0' h='21' lh='30%'> to access your account.</Text>
+                    <TextInput
+                        placeholder="Email"
+                        width='300'
+                        label='Email'
+                        key={form.key('email')}
+                        error={form.errors.email}
+                        styles={{ input: { borderColor: form.errors.email ? 'red' : undefined } }}
+                        {...form.getInputProps('email')}
 
-                />
-                <TextInput
-                    mt="md"
-                    placeholder="Password"
-                    width='300'
-                    label="Password"
-                    type="password"
-                    key={form.key('password')}
-                    error={form.errors.password}
-                    styles={{ input: { borderColor: form.errors.password ? 'red' : undefined } }}
-                    {...form.getInputProps('password')}
-                    mb='lg'
-                />
-                <Button variant="light" color="#A41517" onClick={() => {
-                    router.push('/dashboard');
-                }}> Login in</Button>
+                    />
+                    <TextInput
+                        placeholder="Password"
+                        width='300'
+                        label="Password"
+                        type="password"
+                        key={form.key('password')}
+                        error={form.errors.password}
+                        styles={{ input: { borderColor: form.errors.password ? 'red' : undefined } }}
+                        {...form.getInputProps('password')}
+                        mb='xl'
+                    />
+                    {/* ="#A41517 */}
+                    <Button variant="filled" color='black' onClick={() => {
+                        router.push('/dashboard');
+                    }} justify='center'> Login</Button>
+                </Stack>
             </Card>
         </>
     );
--- interface/src/components/login/LoginPage.tsx
+++ interface/src/components/login/LoginPage.tsx
@@ -1,23 +1,43 @@
 'use client'
 
 
-import { Grid, Flex, rem, Card, AppShell } from '@mantine/core';
+import { Grid, Flex, rem, Card, AppShell, Container, Space, Stack, Group, BackgroundImage } from '@mantine/core';
 import { LoginForm } from '@/components';
+import Image from "next/image";
+import logo from "@/assets/logo_hpc4ai.png"
+import backgroundImage from '@/assets/geometric-maroon-black-red-abstract-art-9zqxwgd44bincdvl.jpg';
+
 
 
 export default function LoginPage() {
+    const containerStyle = {
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    }
 
 
     return (
-        <AppShell>
+        // <AppShell header={{ height: 100 }}>
+        //     <AppShell.Header>
+        //         <Group justify='center' align='center' px='lg'>
+        //             <Image src={logo} alt="HPC4AI" height={70} />
+        //         </Group>
+        //     </AppShell.Header>
 
-            <AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`}>
-                <Grid justify='center' align='center'>
-                    <Grid.Col span={4}>
-                        <LoginForm />
-                    </Grid.Col>
-                </Grid>
-            </AppShell.Main>
-        </AppShell>
+        //     <AppShell.Main >
+        <div className="container" style={containerStyle} >
+            <Container fluid bg='#eeeeee' h='100vh' style={{ width: '100' }}>
+                <Space h="150" />
+                <Stack justify='center' align='center'>
+                    <LoginForm />
+                </Stack>
+            </Container>
+        </div >
+        //     </AppShell.Main>
+        // </AppShell>
     );
 }
