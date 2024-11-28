45ffeb84dd600e7d55b725816fee5bfeb3766ece
Este Hoyos <estehoyos@gmail.com>
2024-11-28 13:25:00 +0100

    style: add background image to login

--- interface/src/components/login/LoginForm.tsx
+++ interface/src/components/login/LoginForm.tsx
@@ -2,7 +2,6 @@
 
 import { Card, Button, TextInput, Group, Text, Stack, Title, Center } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { IconAt } from '@tabler/icons-react';
 import { useRouter } from 'next/navigation';
 import Image from "next/image";
 import logo from "@/assets/logo_hpc4ai.png"
@@ -24,9 +23,9 @@ export default function LoginForm() {
 
     return (
         <>
-            <Card shadow="xl" radius="md" withBorder padding='xl' >
-                <Stack justify='center' align='center' m='xl' >
-                    <Image src={logo} alt="HPC4AI" height={70} />
+            <Card shadow="xl" radius="md" withBorder padding='4em' >
+                <Stack justify='center' align='center' m='xl' mt='xs' >
+                    <Image src={logo} alt="HPC4AI" height={100} />
                     <Group><Title fw='900' size='40' mt='md' mb='md'>Sign</Title><Title fw='900' size='40' c='#A41517'>in</Title> </Group>
                     <Text c='dimmed' mb='1' lineClamp={2} h='21' >Enter your email and password</Text>
                     <Text c='dimmed' mb='md' p='0' h='21' lh='30%'> to access your account.</Text>
@@ -36,8 +35,17 @@ export default function LoginForm() {
                         label='Email'
                         key={form.key('email')}
                         error={form.errors.email}
-                        styles={{ input: { borderColor: form.errors.email ? 'red' : undefined } }}
+                        styles={{
+                            input: {
+
+                                borderColor: form.errors.email ? 'red' : undefined,
+                                '&:focus': { borderColor: 'black' }
+                            }
+                        }}
                         {...form.getInputProps('email')}
+                        w='100%'
+                        size='md'
+                        mb='xs'
 
                     />
                     <TextInput
@@ -50,8 +58,10 @@ export default function LoginForm() {
                         styles={{ input: { borderColor: form.errors.password ? 'red' : undefined } }}
                         {...form.getInputProps('password')}
                         mb='xl'
+                        w='100%'
+                        size='md'
+
                     />
-                    {/* ="#A41517 */}
                     <Button variant="filled" color='black' onClick={() => {
                         router.push('/dashboard');
                     }} justify='center'> Login</Button>
--- interface/src/components/login/LoginPage.tsx
+++ interface/src/components/login/LoginPage.tsx
@@ -1,43 +1,15 @@
-'use client'
-
-
-import { Grid, Flex, rem, Card, AppShell, Container, Space, Stack, Group, BackgroundImage } from '@mantine/core';
+import { Container, Stack, BackgroundImage } from '@mantine/core';
 import { LoginForm } from '@/components';
-import Image from "next/image";
-import logo from "@/assets/logo_hpc4ai.png"
-import backgroundImage from '@/assets/geometric-maroon-black-red-abstract-art-9zqxwgd44bincdvl.jpg';
-
-
+import backgroundImage from '@/assets/geometric-maroon-black-red-abstract-art.jpg';
 
 export default function LoginPage() {
-    const containerStyle = {
-        width: '100vw',
-        height: '100vh',
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-    }
-
-
     return (
-        // <AppShell header={{ height: 100 }}>
-        //     <AppShell.Header>
-        //         <Group justify='center' align='center' px='lg'>
-        //             <Image src={logo} alt="HPC4AI" height={70} />
-        //         </Group>
-        //     </AppShell.Header>
-
-        //     <AppShell.Main >
-        <div className="container" style={containerStyle} >
-            <Container fluid bg='#eeeeee' h='100vh' style={{ width: '100' }}>
-                <Space h="150" />
+        <BackgroundImage src={backgroundImage.src} style={{ width: '100vw', height: '100vh' }}>
+            <Container fluid h='100vh' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Stack justify='center' align='center'>
                     <LoginForm />
                 </Stack>
             </Container>
-        </div >
-        //     </AppShell.Main>
-        // </AppShell>
+        </BackgroundImage>
     );
 }
