87a9fd884f7216169b3ee790a8dab62581094fc7
Elisa Li Perottino <elisa.perottino@edu.unito.it>
2024-12-01 12:53:52 +0100

    style: theme and logo padding
    Co-authored-by: Janneth Hoyos <jannhs@users.noreply.github.com>

--- interface/src/components/AppShell/shell.tsx
+++ interface/src/components/AppShell/shell.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useDisclosure } from '@mantine/hooks';
 import React, { ReactNode } from 'react';
 import CustomNavBar from "../Navbar/navbar";
-import logo from "@/assets/logo_hpc4ai.png"
+import logo from "@/assets/logo_hpc4ai.png";
 
 interface ShellProps {
     children: JSX.Element | ReactNode;
@@ -35,7 +35,7 @@ export const Shell = ({ children }: ShellProps) => {
                         size="sm"
                     />
                     {/* <img src={'/logo_hpc4ai.png'} alt="HPC4AI" height={40} /> */}
-                    <Image src={logo} alt="HPC4AI" height={40} />
+                    <Image src={logo} alt="HPC4AI" height={40} priority style={{margin: '0 0 0 15px'}}/>
                 </Group>
             </AppShell.Header >
 
@@ -45,7 +45,7 @@ export const Shell = ({ children }: ShellProps) => {
 
             <AppShell.Main m='sm'>{children}</AppShell.Main>
 
-        </AppShell >
+        </AppShell>
     );
 }
 
--- interface/theme.ts
+++ interface/theme.ts
@@ -12,4 +12,5 @@ export const theme = createTheme({
             },
         }),
     },
+    primaryColor: 'dark',
 });
