554f433ca3bba5acb3d4af908b48d151edbe8cb0
Este Hoyos <estehoyos@gmail.com>
2024-12-04 10:58:11 +0100

    feat: add keycloak service to docker compose

--- interface/src/utils/authOptions.ts
+++ interface/src/utils/authOptions.ts
@@ -4,12 +4,20 @@ import KeycloakProvider from 'next-auth/providers/keycloak';
 export const authOptions: AuthOptions = {
     providers: [
         KeycloakProvider({
+            jwks_endpoint: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/myrealm/protocol/openid-connect/certs`,
             clientId: process.env.KEYCLOAK_CLIENT_ID,
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
             issuer: process.env.KEYCLOAK_ISSUER,
+            // issuer: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}`,
+            authorization: {
+                params: {
+                    scope: "openid email profile",
+                },
+                url: `${process.env.NEXT_LOCAL_KEYCLOAK_URL}/realms/myrealm/protocol/openid-connect/auth`,
+            },
+            token: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/myrealm/protocol/openid-connect/token`,
+            userinfo: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/myrealm/protocol/openid-connect/userinfo`,
 
-            // realm: process.env.KEYCLOAK_REALM,
-            // serverUrl: process.env.KEYCLOAK_SERVER_URL,
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
--- interface/types/node-env.d.ts
+++ interface/types/node-env.d.ts
@@ -2,6 +2,8 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         KEYCLOAK_CLIENT_ID: string
         KEYCLOAK_CLIENT_SECRET: string
-        KEYCLOAK_ISSUER: string
+        NEXT_LOCAL_KEYCLOAK_URL: string
+        NEXT_PUBLIC_KEYCLOAK_REALM: string
+        NEXT_CONTAINER_KEYCLOAK_ENDPOINT: string
     }
 }
