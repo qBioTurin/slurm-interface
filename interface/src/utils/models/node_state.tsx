d5ac875285913638b439ea054e1cb47f190a3553
Elisa Li Perottino <elisa.perottino@edu.unito.it>
2024-12-16 12:12:00 +0100

    chore(nodes): add missing status to node_state (INVALID_REG)

--- interface/src/utils/models/node_state.tsx
+++ interface/src/utils/models/node_state.tsx
@@ -16,6 +16,7 @@ export const NodeStates: NodeStateInfo[] = [
     { state: 'FUTURE', description: 'The node is currently not fully configured, but expected to be available at some point in the indefinite future for use.' },
     { state: 'IDLE', description: 'The node is not allocated to any jobs and is available for use.' },
     { state: 'INVAL', description: "The node did not register correctly with the controller. This happens when a node registers with less resources than configured in the slurm.conf file. The node will clear from this state with a valid registration (i.e. a slurmd restart is required)." },
+    { state: 'INVALID_REG', description: "The node did not register correctly with the controller. This happens when a node registers with less resources than configured in the slurm.conf file." },
     { state: 'MAINT', description: 'The node is currently in a reservation with a flag value of "maintenance".' },
     { state: 'REBOOT_ISSUED', description: 'A reboot request has been sent to the agent configured to handle this request.' },
     { state: 'REBOOT_REQUESTED', description: "A request to reboot this node has been made, but hasn't been handled yet." },
@@ -28,4 +29,4 @@ export const NodeStates: NodeStateInfo[] = [
     { state: 'POWERING_UP', description: 'The node is in the process of being powered up.' },
     { state: 'RESERVED', description: 'The node is in an advanced reservation and not generally available.' },
     { state: 'UNKNOWN', description: "The Slurm controller has just started and the node's state has not yet been determined." },
-  ] as const;
+] as const;
